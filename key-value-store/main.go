9acfcdce5d4e4e5abe42cfb8ce978c4980970234
khang nguyen <keirames.l3@gmail.com>
2024-05-02 22:00:52 +0700

    consensus default

--- key-value-store/main.go
+++ key-value-store/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"net"
+	"main/raft"
 	"time"
 )
 
@@ -14,67 +14,12 @@ func keepProcessAlive() {
 	}
 }
 
-func createServerWithPeers(addr string, peers []string) {
-	listener, err := net.Listen("tcp", addr)
-	if err != nil {
-		fmt.Println("Server listen tcp err:", err)
-		return
-	}
-	defer listener.Close()
-
-	for {
-		fmt.Println("Waiting incomming connection...")
-		// Accept incomming connection
-		conn, err := listener.Accept()
-		if err != nil {
-			fmt.Println("Accept incomming connection err:", err)
-			continue
-		}
-
-		go func() {
-			defer conn.Close()
-
-			// Create buffer to read data
-			buffer := make([]byte, 1024)
-
-			for {
-				fmt.Println("Waiting to read data from client...")
-				// Read data from client
-				n, err := conn.Read(buffer)
-				if err != nil {
-					fmt.Println("Read data from client err:", err)
-					return
-				}
-
-				// Process and use the data (here, we'll just print it)
-				fmt.Printf("Received: %s\n", buffer[:n])
-			}
-		}()
-
-		// Connect to peers
-		for _, p := range peers {
-			conn, err := net.Dial("tcp", p)
-			if err != nil {
-				fmt.Println("Connect to peer fail", err)
-				return
-			}
-		}
-	}
-}
-
 func main() {
-	addrs := []string{"localhost:5000", "localhost:5001", "localhost:5002"}
+	cm1 := raft.New(1, raft.Leader)
+	cm2 := raft.New(2, raft.Follower)
+	cm3 := raft.New(3, raft.Follower)
 
-	for _, addr := range addrs {
-		peers := []string{}
-		for _, pAddr := range addrs {
-			if pAddr != addr {
-				peers = append(peers, pAddr)
-			}
-		}
-
-		go createServerWithPeers(addr, peers)
-	}
+	fmt.Println(cm1, cm2, cm3)
 
 	keepProcessAlive()
 }
