49b066ca997ac3b14468f04c5d6f302e1e7d2ea7
Nguyen Manh Khang <khang.nguyen@HD-KHANGNM.local>
2024-05-02 16:32:26 +0700

    init

--- key-value-store/main.go
+++ key-value-store/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"time"
 )
 
@@ -13,11 +14,66 @@ func keepProcessAlive() {
 	}
 }
 
+func createServerWithPeers(addr string, peers []string) {
+	listener, err := net.Listen("tcp", addr)
+	if err != nil {
+		fmt.Println("Server listen tcp err:", err)
+		return
+	}
+	defer listener.Close()
+
+	for {
+		fmt.Println("Waiting incomming connection...")
+		// Accept incomming connection
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Accept incomming connection err:", err)
+			continue
+		}
+
+		go func() {
+			defer conn.Close()
+
+			// Create buffer to read data
+			buffer := make([]byte, 1024)
+
+			for {
+				fmt.Println("Waiting to read data from client...")
+				// Read data from client
+				n, err := conn.Read(buffer)
+				if err != nil {
+					fmt.Println("Read data from client err:", err)
+					return
+				}
+
+				// Process and use the data (here, we'll just print it)
+				fmt.Printf("Received: %s\n", buffer[:n])
+			}
+		}()
+
+		// Connect to peers
+		for _, p := range peers {
+			conn, err := net.Dial("tcp", p)
+			if err != nil {
+				fmt.Println("Connect to peer fail", err)
+				return
+			}
+		}
+	}
+}
+
 func main() {
 	addrs := []string{"localhost:5000", "localhost:5001", "localhost:5002"}
 
 	for _, addr := range addrs {
-		fmt.Println(addr)
+		peers := []string{}
+		for _, pAddr := range addrs {
+			if pAddr != addr {
+				peers = append(peers, pAddr)
+			}
+		}
+
+		go createServerWithPeers(addr, peers)
 	}
 
 	keepProcessAlive()
