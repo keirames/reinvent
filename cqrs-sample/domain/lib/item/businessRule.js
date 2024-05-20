dc6f137d2123572dfbbf193a0608678c887f2c15
khang nguyen <keirames.l3@gmail.com>
2024-05-20 14:14:57 +0700

    clean

--- cqrs-sample/domain/lib/item/businessRule.js
+++
@@ -1,7 +0,0 @@
-module.exports = require('cqrs-domain').defineBusinessRule({
-  name: 'checkForError'
-}, function (changed, previous, events, command) {
-  if (changed.get('text').toLowerCase().indexOf('error') >= 0) {
-    throw new Error('This is just a sample rule!');
-  }
-});
