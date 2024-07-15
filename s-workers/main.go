b0481f3af59c37d7ba045e5cfc553acb84cdbc71
khang nguyen <keirames.l3@gmail.com>
2024-07-15 21:59:09 +0700

    chore: reids connection

--- s-workers/main.go
+++ s-workers/main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"context"
+
+	"github.com/redis/go-redis/v9"
+)
+
 func main() {
 	// somehow get msg to process
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+	rdb.Ping(context.Background())
 }
