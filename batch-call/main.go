1d2c763e0587d8d71357d8d162675db907746186
khang nguyen <keirames.l3@gmail.com>
2024-07-23 00:27:43 +0700

    only 1 consumer

--- batch-call/main.go
+++ batch-call/main.go
@@ -1,56 +1,10 @@
 package main
 
-import (
-	"context"
-	"log"
-	"time"
-
-	amqp "github.com/rabbitmq/amqp091-go"
-)
-
 func main() {
 	rmq, err := New()
 	if err != nil {
 		panic(err)
 	}
 
-	ch, err := rmq.conn.Channel()
-	if err != nil {
-		panic(err)
-	}
-
-	q, err := ch.QueueDeclare(
-		"hello", // name
-		false,   // durable
-		false,   // delete when unused
-		false,   // exclusive
-		false,   // no-wait
-		nil,     // arguments
-	)
-	if err != nil {
-		panic(err)
-	}
-
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
-	body := "Hello World!"
-	err = ch.PublishWithContext(ctx,
-		"",     // exchange
-		q.Name, // routing key
-		false,  // mandatory
-		false,  // immediate
-		amqp.Publishing{
-			ContentType: "text/plain",
-			Body:        []byte(body),
-		})
-	if err != nil {
-		panic(err)
-	}
-	log.Printf(" [x] Sent %s\n", body)
-
-	go rmq.Consume()
-
-	var forever chan struct{}
-	<-forever
+	rmq.Consume()
 }
