4c0cb42a54540b75f06fd31f1484c3fa6b5d41fe
khang nguyen <keirames.l3@gmail.com>
2024-08-07 20:09:47 +0700

    ants test

--- batch-call/bull-go/go.mod
+++ batch-call/bull-go/go.mod
@@ -1,3 +1,7 @@
 module main
 
 go 1.22.2
+
+require github.com/panjf2000/ants/v2 v2.10.0
+
+require golang.org/x/sync v0.3.0 // indirect
--- batch-call/bull-go/main.go
+++ batch-call/bull-go/main.go
@@ -3,20 +3,37 @@ package main
 import (
 	"fmt"
 	"time"
+
+	"github.com/panjf2000/ants/v2"
 )
 
 type Worker struct {
 }
 
-func task() {
-	for range 10 {
-		fmt.Println("task")
-	}
-}
-
 func main() {
-	p := NewPool()
-	fmt.Println(p)
+	p, err := ants.NewPoolWithFunc(10, func(i interface{}) {
+		time.Sleep(time.Millisecond * 500)
+		v, ok := i.(int)
+		if !ok {
+			panic("not ok")
+		}
+
+		fmt.Println(v)
+	})
+	defer p.Release()
+
+	if err != nil {
+		fmt.Println("Failed to initiate goroutine pool")
+		panic(err)
+	}
+
+	// push 1000 jobs
+	for i := range 1000 {
+		err := p.Invoke(i)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
 
 	time.Sleep(time.Second * 5)
 }
