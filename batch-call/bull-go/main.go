e51cc9812192488ad3acdf59bec94bfa6cb751b6
khang nguyen <keirames.l3@gmail.com>
2024-08-07 19:29:42 +0700

    bull clone

--- batch-call/bull-go/main.go
+++ batch-call/bull-go/main.go
@@ -1,5 +1,44 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"time"
+)
 
+type Pool struct {
+	capacity int32
+
+	workers []int
+}
+
+func (p *Pool) Submit(task func()) error {
+	fmt.Println("received task")
+
+	func() {
+		task()
+	}()
+
+	return nil
+}
+
+func task() {
+	for range 10 {
+		fmt.Println("task")
+	}
+}
+
+func main() {
+	p := &Pool{
+		capacity: 10,
+		workers:  []int{},
+	}
+	for i := range 10 {
+		fmt.Println(i)
+		err := p.Submit(task)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+
+	time.Sleep(time.Second * 5)
 }
